5570cbab4d8599946b7d7ecfbf41a4a720f71b8b
Kraus, Simon <simon.kraus@aol.com>
2019-07-24 22:29:24 -0500

    basic functionality

---
+++ data/friends.js
@@ -0,0 +1,18 @@
+var friendsArr = [
+    {
+        "name": "Theodore",
+        "photo": "https://en.wikipedia.org/wiki/Theodore_Roosevelt#/media/File:President_Roosevelt_-_Pach_Bros.jpg",
+        "scores": [
+            5,
+            4,
+            5,
+            4,
+            5,
+            4,
+            5,
+            4,
+            5,
+            4
+        ]
+    }
+]
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
+
+
+app.listen(PORT, function(){
+    console.log("Listening on port: " + PORT)
+});
+
